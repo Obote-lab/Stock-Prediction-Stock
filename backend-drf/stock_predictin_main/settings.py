61f4d40336704755a086cbba30d05767f94188ad
Obote-lab <kevinomondi414@gmail.com>
2025-07-30 17:34:17 +0300

    registration api

--- backend-drf/stock_predictin_main/settings.py
+++ backend-drf/stock_predictin_main/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'api',
+    'accounts',
+
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
